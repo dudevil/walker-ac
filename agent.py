f4e88181d608f3701d493b7e045d8bba6a81290e
Sergey <ovcharenko.ser@gmail.com>
2016-12-27 20:04:29 +0300

    Add random strategy

---
+++ agent.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import gym
+
+env = gym.make('BipedalWalker-v2')
+env.reset()
+
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
